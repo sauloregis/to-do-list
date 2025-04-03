5032a80eb17a38036fd9b2633ff4c3ec657cce51
Saulo Regis de Medeiros Costa <l4910@liga.org.br>
2025-04-03 08:23:13 -0300

    Ajuste de dependências do tailWind. Já está criando e deletando tarefas

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // Adicione esta linha se estiver usando o diretório `app`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
